bbf1c244b68e37532999de62cdb92b294f050aa3
kimhart <kim@jwplayer.com>
2019-07-16 17:43:53 -0400

    redo project hover, needs icon + sunrise theme

--- src/components/project.js
+++ src/components/project.js
@@ -8,11 +8,21 @@ export default class Project extends React.Component {
     return (
       <div className={`project`}>
         <a href={link} target="_blank">
-          {gif && <img src={`${src}`} alt={alt} className="gif"/>}
+          {gif && <img src={`${src}`} alt={alt} className="gif" />}
           {!gif && <Image {...this.props} />}
           <div className="project__info">
-            <div className="project__title">{title}</div>
+            <div className="project__title-wrap">
+              <div className="project__title">{title}</div>
+              <div className="project__shadow">{title}</div>
+            </div>
             <div className="project__description">{description}</div>
+            <svg className="project__corner" viewBox="0 0 540 270" xmlns="http://www.w3.org/2000/svg">
+              <g transform="translate(-170.000000, -1404.000000)" fill="#678EF8">
+                <g transform="translate(170.000000, 1134.000000)">
+                  <polygon points="540 270 0 540 540 540"></polygon>
+                </g>
+              </g>
+            </svg>
           </div>
         </a>
       </div>
