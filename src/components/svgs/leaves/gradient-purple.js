e630e761be5b314aeb9ac9b48b100ad244b83ab7
Kim Hart <kimhart@Kims-Laptop.local>
2023-06-09 15:51:52 -0400

    plant leaves

---
+++ src/components/svgs/leaves/gradient-purple.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const PurpleGradient = () => {
+  return (
+    <radialGradient id="purpleGradient">
+      <stop offset="15%" stopColor="#9d77ff" />
+      <stop offset="80%" stopColor="#4800ff" />
+    </radialGradient>
+  )
+}
+
+export default PurpleGradient;
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,9 @@ import Image from "../components/image"
 import Github from "../components/svgs/github"
 import LinkedIn from "../components/svgs/linkedin"
 import Twitter from "../components/svgs/twitter"
+import Leaf6 from "../components/svgs/leaves/leaf-6"
+import Leaf1 from "../components/svgs/leaves/leaf-1"
+import Leaf2 from "../components/svgs/leaves/leaf-2"
 
 const IndexPage = () => {
 
@@ -26,17 +29,24 @@ const IndexPage = () => {
       <SEO title="Kim Hart | Frontend Engineer + Design Technologist" />
       <div className="home">
         <div className="hero">
-          <h1>{renderNeonName()}</h1>
-          <h2>
-            frontend engineer
-            <span>&bull;</span> 
-            design technologist
-            <span>&bull;</span>
-            brooklyn, ny
-          </h2>
+          <div className="title">
+            <h1>{renderNeonName()}</h1>
+            <h2>
+              frontend engineer
+              <span>&bull;</span> 
+              design technologist
+              <span>&bull;</span>
+              brooklyn, ny
+            </h2>
+          </div>
+          <div className="leaves">
+            <Leaf6 rotate={20} width={400} />
+            <Leaf1 rotate={-10} width={200} />
+            <Leaf2 rotate={-10} width={200} />
+          </div>
         </div>
 
-        <div className="about">
+        {/* <div className="about">
           <h3>deets</h3>
           <div className="headshot">
             <Image src="kim-headshot.png" alt="Headshot of Kim" />
@@ -70,10 +80,10 @@ const IndexPage = () => {
             </li>
             <li>Probably cuddling my <a href="https://www.instagram.com/spicymargbk/" target="_blank" rel="noopener nooreferrer">red golden retriever</a> right now</li>
           </ul>
-        </div>
+        </div> */}
 
         <div className="projects">
-          <h3>some pretty stuff</h3>
+          {/* <h3>some pretty stuff</h3>
           <h2>
             <span>[</span>
             react
@@ -87,34 +97,32 @@ const IndexPage = () => {
             vite
             <span>&bull;</span>
             jest
-            <span>&bull;</span>
-
             <span>]</span>
-          </h2>
+          </h2> */}
           <div className="product-shots">
             <div>
               <Image src="product-shots/player-customization.png" alt="2 product shots of a video player customization suite that allows users to adjust colors, caption styles, menus and logos in the JW Player" />
-              <p>JWP video player customization suite</p>
+              {/* <p>JWP video player customization suite</p> */}
             </div>
 
             <div>
               <Image src="product-shots/trimming-tool.png" alt="3 product shots of a media trimming tool featuring a video of a skateboarder in Venice Beach, an audio file of a podcast" />
-              <p>Media trimming & clipping tool</p>
+              {/* <p>Media trimming & clipping tool</p> */}
             </div>
 
             <div>
               <Image src="product-shots/ui-components.png" alt="Collage of screenshots of web components (form inputs, draggable lists, thumbnails, buttons, alerts, etc)" />
-              <p>Design system + web components</p>
+              {/* <p>Design system + custom web component library</p> */}
             </div>
 
             <div>
               <Image src="product-shots/scout.png" alt="Collage of screenshots of chrome extension" />
-              <p>Chrome extension for QA testing UI elements</p>
+              {/* <p>Chrome extension for QA testing UI elements</p> */}
             </div>
 
             <div>
               <Image src="product-shots/jw-music.png" alt="Collage of screenshots of custom music player" />
-              <p>Custom music app powered by Spotify</p>
+              {/* <p>Hackweek music app powered by Spotify</p> */}
             </div>
           </div>
         </div>
