9135c0813506541681d259cbcae2f332b9bd2c5b
Kim Hart <kimhart@Kims-Laptop.local>
2023-07-02 16:39:45 -0400

    popart redesign

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,193 +1,114 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+import { StaticImage } from "gatsby-plugin-image"
+import { Plane } from "../components/svg/plane"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
+    <main>
+      <section className="hero">
+        <h1>
+          hey there, i'm
+          <div>Kim Hart</div>
+        </h1>
+        <h2>
+          Frontend Engineer + Design Technologist
+        </h2>
+      </section>
+
+      <section className="about">
+        <div className="headshot">
+          <StaticImage 
+            src="../images/misc/kim-headshot.png" 
+            alt="Headshot of Kim"
+            width={200}
+          />
+        </div>
+        <div className="bio">
+          <h3>The Basics</h3>
+          <ul>
+            <li>
+              <strong>Frontend team lead</strong> with 6+ years of experience building <a target="_blank" rel="noopener noreferrer" href="https://dashboard.jwplayer.com">enterprise products</a>
+            </li>
+            <li>React and Typescript engineer</li>
+            <li>
+              Specialist in <a target="_blank" rel="noopener noreferrer" href="https://medium.com/jw-player-engineering/a-real-life-roadmap-for-building-a-design-system-pt-i-a805d81bd90">design systems</a> and <a target="_blank" rel="noopener noreferrer" href="https://medium.com/jw-player-engineering/a-real-life-roadmap-for-building-a-design-system-pt-ii-c4c2490875fe">web component libraries</a>
+            </li>
+            <li>
+              Creative thinker with a journalism degree and a passion for art & design
+            </li>
+            <li>Former mentor & speaker for {" "}
               <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
+                href="https://www.builtbygirls.com/about-wave"
+                target="_blank"
               >
-                {link.text}
+                BuiltByGirls
               </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+              ,{" "}
+              <a
+                href="https://medium.com/jw-player-engineering/advice-for-getting-a-career-in-engineering-8bdb653b3837"
+                target="_blank"
+              >
+                Uncubed
+              </a>{" "}
+              and{" "}
+              <a href="https://codenation.org/" target="_blank">
+                CodeNation
+              </a>{" "}
+            </li>
+            <li>Probably hanging out with my <a href="https://www.instagram.com/spicymargbk/" target="_blank" rel="noopener nooreferrer">red golden retriever</a> right now</li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="contact">
+        <a href="https://linkedin.com/in/kimhart1" target="_blank" rel="noopener nooreferrer"><h4>get in touch <Plane/></h4></a>
+      </div>
+
+      <section className="work">
+        <h3>Some Pretty Stuff</h3>
+        <div className="projects">
+          <div className="project">
+            <StaticImage 
+              src="../images/product-shots/trimming-tool.png" 
+              alt="Product shots of JW Dashboard in which a user is trimming a skate video and a podcast audio"
+              />
+            <p>Media trimming and clipping tool</p>
+          </div>
+          <div className="project">
+            <StaticImage 
+              src="../images/product-shots/ui-components.png" 
+              alt="Examples of components like inputs, buttons, draggable episode lists, and alerts"
+              />
+            <p>Custom web component library</p>
+          </div>
+          <div className="project">
+            <StaticImage 
+              src="../images/product-shots/player-customization.png" 
+              alt="Product shots of JW Dashboard in which user is editing logos and colors used in JW Player"
+              />
+            <p>Video player branding & customization suite</p>
+          </div>
+          <div className="project">
+            <StaticImage 
+              src="../images/product-shots/scout.png" 
+              alt="Scout highlighting elements with test IDs in the browser"
+              />
+            <p>Scout: Chrome extension for fast & easy QA testing</p>
+          </div>
+          <div className="project">
+            <StaticImage 
+              src="../images/product-shots/jw-music.png" 
+              alt="Custom music app powered by Spotify"
+              />
+            <p>Custom music app powered by Spotify</p>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Kim Hart | Frontend Engineer</title>
