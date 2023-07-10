21ff8c8dd3bffd9ebde42f834923caff7f820b4f
Kim Hart <kimhart@Kims-Laptop.local>
2023-07-10 12:44:22 -0400

    update description, move preview image to static

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,9 +3,9 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: "Kim Hart | Frontend Engineer",
-    description: "Creative software engineer & design technologist based in Brooklyn, NY",
+    description: "Creative dev & design technologist based in Brooklyn, NY",
     twitterUsername: "@kimhart",
-    image: "src/images/misc/preview-image.png",
+    image: "/preview-image.png",
     siteUrl: "https://kimhart.nyc",
   },
   graphqlTypegen: true,
